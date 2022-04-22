db12424bfc2d4cecbcbb2272b46d50746d811042
Antoine <27826950+Antoine-lb@users.noreply.github.com>
2022-04-22 16:28:41 +0200

    asdf

--- detect.py
+++ detect.py
@@ -80,7 +80,7 @@ def run(model: str, camera_id: int, width: int, height: int, num_threads: int,
     print("===")
     print(detections[0])
     print(detections[0].categories[0].label)
-    if (detections[0].categories[0].label == "person")
+    if (detections[0].categories[0].label == "person"):
       print ("keep going")
     else
       print ("Don't print")
