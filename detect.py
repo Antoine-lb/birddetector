ce745b14cb8447f5b955b820f84efbdd3ad8596c
Antoine <27826950+Antoine-lb@users.noreply.github.com>
2022-04-25 09:46:36 +0200

    asdfs

--- detect.py
+++ detect.py
@@ -90,7 +90,7 @@ def run(model: str, camera_id: int, width: int, height: int, num_threads: int,
     tolerance = 30
     half_w = width / 2
     print("half_w", half_w)
-    cv2.rectangle(image, pt1=(250,200), pt2=(300,300), color=(255,0,0), thickness=1)
+    cv2.rectangle(image, pt1=(300,220), pt2=(340,260), color=(255,0,0), thickness=1)
 
     # Calculate the FPS
     if counter % fps_avg_frame_count == 0:
